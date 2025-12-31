2ac0c04f947cf927ee1ccb4ef2a4bd99327b8767
dimkpapadopoulos <dimkpapadopoulos@gmail.com>
2025-12-31 19:27:19 +0200

    Ability to list all entries and delete. Data now encrypted.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
+use argon2::Argon2;
 use bincode;
+use chacha20poly1305::{
+    ChaCha20Poly1305,
+    Nonce,
+    aead::{Aead, KeyInit}, // Traits for encryption
+};
+use rand::{RngCore, rngs::OsRng};
 use rpassword;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs::File;
-use std::io;
+use std::io::{Read, Write};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Entry {
@@ -15,28 +22,63 @@ pub struct Entry {
 
 pub fn input() -> String {
     let mut input = String::new();
-    io::stdin()
+    std::io::stdin()
         .read_line(&mut input)
         .expect("Failed to get input.");
-    let trimmed = input.trim();
-    trimmed.to_string()
+    input.trim().to_string()
 }
 
-pub fn save_to_file(vault: &HashMap<String, Entry>, filename: &str) {
-    let file = File::create(filename).expect("Could not write file to disk.");
-    bincode::serialize_into(file, vault).expect("Failed to save data.");
+pub fn save_to_file(vault: &HashMap<String, Entry>, filename: &str, master_pwd: &str) {
+    let mut file = File::create(filename).expect("Could not write file to disk.");
+    let data_bytes = bincode::serialize(vault).expect("Failed to serialize data.");
+    let mut salt = [0u8; 16];
+    OsRng.fill_bytes(&mut salt);
+    let key = derive_key(master_pwd, &salt);
+    let cipher = ChaCha20Poly1305::new_from_slice(&key).expect("Invalid key length.");
+    let mut nonce_bytes = [0u8; 12];
+    OsRng.fill_bytes(&mut nonce_bytes);
+    let nonce = Nonce::from_slice(&nonce_bytes);
+    let encrypted_data = cipher
+        .encrypt(nonce, data_bytes.as_ref())
+        .expect("Failed to encrypt data.");
+    file.write_all(&salt).expect("Failed to write salt");
+    file.write_all(&nonce_bytes).expect("Failed to write nonce");
+    file.write_all(&encrypted_data)
+        .expect("Failed to write encrypted data");
 }
 
-pub fn load_from_file(filename: &str) -> HashMap<String, Entry> {
-    let file = match File::open(filename) {
+pub fn load_from_file(filename: &str, master_pwd: &str) -> HashMap<String, Entry> {
+    let mut file = match File::open(filename) {
         Ok(f) => f,
-        Err(_) => return HashMap::new(), // File not found? Return empty map immediately
+        Err(_) => return HashMap::new(),
     };
 
-    // If we get here, the file exists, so we try to load the data
-    bincode::deserialize_from(file).expect("Failed to load data")
+    let mut salt = [0u8; 16];
+    match file.read_exact(&mut salt) {
+        Ok(_) => {}
+        Err(_) => return HashMap::new(),
+    }
+
+    let key = derive_key(master_pwd, &salt);
+
+    let mut nonce_bytes = [0u8; 12];
+    file.read_exact(&mut nonce_bytes)
+        .expect("Failed to read nonce");
+    let nonce = Nonce::from_slice(&nonce_bytes);
+
+    let mut encrypted_data = Vec::new();
+    file.read_to_end(&mut encrypted_data)
+        .expect("Failed to read ciphertext");
+
+    let cipher = ChaCha20Poly1305::new_from_slice(&key).expect("Invalid Key");
+    let decrypted_bytes = match cipher.decrypt(nonce, encrypted_data.as_ref()) {
+        Ok(bytes) => bytes,
+        Err(_) => panic!("Incorrect Password or Corrupted Data!"),
+    };
+
+    bincode::deserialize(&decrypted_bytes).expect("Failed to load data")
 }
-pub fn add(vault: &mut HashMap<String, Entry>) {
+pub fn add(vault: &mut HashMap<String, Entry>, master_pwd: &str) {
     println!("Please enter the name of the entry.");
     let name = input();
     println!("Please enter the url of the entry.");
@@ -57,7 +99,7 @@ pub fn add(vault: &mut HashMap<String, Entry>) {
         password,
     };
     let old_value = vault.insert(new_entry.name.clone(), new_entry);
-    save_to_file(vault, "passwords.bin");
+    save_to_file(vault, "passwords.bin", master_pwd);
     match old_value {
         Some(_) => {
             println!("Entry updated.\n")
@@ -78,11 +120,6 @@ pub fn get(vault: &HashMap<String, Entry>) {
                 "Name: {}\nUrl: {}\nUsername: {}\nPassword: {}",
                 entry.name, entry.url, entry.username, entry.password
             );
-            println!("Press enter to return to main screen.");
-            loop {
-                input();
-                break;
-            }
         }
         None => {
             println!("No entry found under this name.\n")
@@ -90,22 +127,32 @@ pub fn get(vault: &HashMap<String, Entry>) {
     }
 }
 pub fn list(vault: &HashMap<String, Entry>) {
-    println!("Stored passwords: {}", vault.capacity());
+    println!("Stored passwords: {}", vault.len());
     let mut i = 0;
     for (name, _entry) in vault {
         i += 1;
-        println!("{}. {}", i, name);
+        println!("{}. {}\n", i, name);
     }
 }
-pub fn delete(vault: &mut HashMap<String, Entry>) {
+pub fn delete(vault: &mut HashMap<String, Entry>, master_pwd: &str) {
     println!("Please enter the name of the entry you want to delete.");
     let name = input();
     let outcome = vault.remove(&name.clone());
     match outcome {
         Some(_) => {
-            save_to_file(vault, "passwords.bin");
+            save_to_file(vault, "passwords.bin", &master_pwd);
             println!("Entry deleted succesfully.");
         }
         None => println!("Entry not found in vault."),
     }
+}
+
+fn derive_key(password: &str, salt: &[u8]) -> [u8; 32] {
+    let mut key = [0u8; 32];
+
+    let argon2 = Argon2::default();
+    argon2
+        .hash_password_into(password.as_bytes(), salt, &mut key)
+        .expect("Failed to derive key.");
+    key
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,48 @@
-use password_manager::{Entry, add, get, input, load_from_file};
-use std::collections::HashMap;
+use clearscreen;
+use password_manager::{Entry, add, delete, get, input, list, load_from_file};
+use std::{collections::HashMap, io::Write};
 
 fn main() {
-    let mut vault: HashMap<String, Entry> = load_from_file("passwords.bin");
+    let master_pwd = rpassword::prompt_password("Enter your master password: ")
+        .expect("Failed to get master password.");
+    clearscreen::clear().expect("failed to clear screen.");
+    let mut vault: HashMap<String, Entry> = load_from_file("passwords.bin", &master_pwd);
 
     loop {
-        println!(
-            "This is the CLI password manager. Please choose between the following actions:\n   1. add: Create a new entry in the password vault.\n   2. get: Retrieve an entry from the password vault.\n   3. quit: exit the program."
+        print!(
+            "This is the CLI password manager. Please type a command to continue \n(for a list of available commands type \"help\": "
         );
+        let _ = std::io::stdout().flush();
         let command = input();
         match command.as_str() {
-            "a" | "A" | "add" | "ADD" | "Add" | "1" => add(&mut vault),
+            "a" | "A" | "add" | "ADD" | "Add" | "1" => add(&mut vault, &master_pwd),
 
             "g" | "G" | "get" | "GET" | "Get" | "2" => get(&vault),
 
-            "q" | "Q" | "quit" | "QUIT" | "Quit" | "3" => {
+            "d" | "D" | "delete" | "DELETE" | "Delete" | "3" => delete(&mut vault, &master_pwd),
+
+            "l" | "L" | "list" | "LIST" | "List" | "4" => list(&vault),
+
+            "q" | "Q" | "quit" | "QUIT" | "Quit" | "5" => {
                 println!("Now exiting the password manager.");
                 break;
             }
-
+            "h" | "H" | "help" | "HELP" | "Help" => {
+                println!(
+                    "List of available commands:
+            1. add: Create a new entry in the password vault.
+            2. get: Retrieve an entry from the password vault.
+            3. delete: Delete an entry from the password vault.
+            4. list: Lists all entries in the vault.
+            5. quit: exit the program."
+                );
+            }
             _ => {
-                println!("Unknown command. Try one of add, get or quit.");
+                println!("Unknown command.");
             }
         }
+        println!("Press enter to go back to menu.");
+        input();
+        clearscreen::clear().expect("failed to clear screen");
     }
 }
